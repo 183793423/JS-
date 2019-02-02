27c183c9adba9d30cd01ec3ccdb18a10698c6792
183793423 <42922026+183793423@users.noreply.github.com>
2019-02-02 19:01:02 +0800

    Add files via upload

---
+++ 年会抽奖/lottery-master/js/member.js
@@ -0,0 +1,16 @@
+var member = [
+  {
+    "phone": "No.5311",
+    "name": "如意"
+  },
+  {
+    "phone": "No.5311",
+    "name": "巨人"
+  },{
+    "phone": "No.5311",
+    "name": "百度"
+  },{
+    "phone": "No.5311",
+    "name": "谷歌"
+  }
+]
